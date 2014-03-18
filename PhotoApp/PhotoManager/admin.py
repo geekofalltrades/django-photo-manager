2b0bcca712351977e80711487f9fd63d285d2899
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-18 15:04:40 -0700

    Registered models for use in the admin.

--- PhotoApp/PhotoManager/admin.py
+++ PhotoApp/PhotoManager/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from PhotoManager.models import Tag, Photo, Album
 
-# Register your models here.
+admin.site.register(Tag)
+admin.site.register(Photo)
+admin.site.register(Album)
