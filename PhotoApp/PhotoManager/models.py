44c0bbfc35036c621e86b9e4357c881db0cd8839
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-18 15:30:30 -0700

    Added title field to photos and made certain fields non-required. Created a migration to reflect the changes and applied it.

--- PhotoApp/PhotoManager/models.py
+++ PhotoApp/PhotoManager/models.py
@@ -9,23 +9,33 @@ class Tag(models.Model):
     text = models.CharField(max_length=32)
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def __unicode__(self):
+        return self.text
+
 
 class Photo(models.Model):
     """An individual photograph. This photo may exist in many albums and
     have many tags.
     """
+    title = models.TextField(max_length=64, default='Untitled')
     timestamp = models.DateTimeField(auto_now_add=True)
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, blank=True)
     description = models.TextField()
     author = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.title
 
 
 class Album(models.Model):
     """A photo album. Albums may contain many photos, and these photos django
     not need to be unique to this album.
     """
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=64)
     description = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    photos = models.ManyToManyField(Photo)
+    photos = models.ManyToManyField(Photo, blank=True)
     author = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.title
