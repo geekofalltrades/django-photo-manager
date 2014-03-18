55a77f6df800483cc2259a46f8c16c2d8dbe09c4
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-18 14:45:18 -0700

    Added models for Tag, Photo, and Album.

--- PhotoApp/PhotoManager/models.py
+++ PhotoApp/PhotoManager/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    """A tag. Any tag can be applied to multiple photos, and those photos
+    may have multiple tags.
+    """
+    text = models.CharField(max_length=32)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Photo(models.Model):
+    """An individual photograph. This photo may exist in many albums and
+    have many tags.
+    """
+    timestamp = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+    description = models.TextField()
+    author = models.ForeignKey(User)
+
+
+class Album(models.Model):
+    """A photo album. Albums may contain many photos, and these photos django
+    not need to be unique to this album.
+    """
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    photos = models.ManyToManyField(Photo)
+    author = models.ForeignKey(User)
