f8fd86b6ca0256808f5f5150eecc10e3cfe9fec2
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-19 14:02:51 -0700

    Modified models to have date_created and date_added, and removed title field from image. Added an ImageField to the image, so it can actually be an image.

--- PhotoManager/models.py
+++ PhotoManager/models.py
@@ -7,21 +7,31 @@ class Tag(models.Model):
     may have multiple tags.
     """
     text = models.CharField(max_length=32)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return self.text
+
+
+def set_upload_to(instance, filename):
+    """Determine the folder where an image will be uploaded. Images go
+    into folders that are named for their author's primary key id. This
+    gives each user a folder while avoiding the problem of usernames that
+    contain characters that can't go in file/folder names.
+    """
+    return '/%d/%s' % (instance.author.pk, filename)
 
 
 class Photo(models.Model):
     """An individual photograph. This photo may exist in many albums and
     have many tags.
     """
-    title = models.TextField(max_length=64, default='Untitled')
-    timestamp = models.DateTimeField(auto_now_add=True)
-    tags = models.ManyToManyField(Tag, blank=True, null=True)
+    image = models.ImageField(upload_to=set_upload_to)
     description = models.TextField(blank=True)
     author = models.ForeignKey(User)
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return self.title
@@ -33,9 +43,10 @@ class Album(models.Model):
     """
     title = models.CharField(max_length=64)
     description = models.TextField(blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
     photos = models.ManyToManyField(Photo, blank=True, null=True)
     author = models.ForeignKey(User)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return self.title
