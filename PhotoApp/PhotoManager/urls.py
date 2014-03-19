1c1f9c9888961b93ec497186c441f6adf08e8507
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-19 13:07:57 -0700

    Finished cleaning up previous directory structure from repository.

--- PhotoApp/PhotoManager/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns(
-    'PhotoManager.views',
-    # Examples:
-    # url(r'^$', 'PhotoApp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-)
