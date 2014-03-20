166a7f2bbd775ae06c5c5f3272ab95f1ee416510
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-20 16:07:19 -0700

    Added urls for all views. Added skeleton views. Added ModelForms for all models.

--- PhotoManager/urls.py
+++ PhotoManager/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     'PhotoManager.views',
-    # Examples:
-    # url(r'^$', 'PhotoApp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'PhotoApp.views.frontpage_view', name='frontpage'),
+    url(r'^home/$', 'PhotoApp.views.home_view', name='home'),
+    url(r'^album/(?P<id>\d+)$', 'PhotoApp.views.album_view', name='album'),
+    url(r'^photo/(?P<id>\d+)$', 'PhotoApp.views.photo_view', name='photo'),
+    url(r'^tag/(?P<id>\d+)$', 'PhotoApp.views.tag_view', name='tag'),
+    url(r'^login/$', 'PhotoApp.views.login_view', name='login'),
+    url(r'^logout/$', 'PhotoApp.views.logout_view', name='logout'),
 )
--- PhotoManager/views.py
+++ PhotoManager/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from models import Tag, Photo, Album
+from django.forms import ModelForm
 
-# Create your views here.
+
+class TagForm(ModelForm):
+    class Meta(object):
+        model = Tag
+        fields = ['text']
+
+
+class PhotoForm(ModelForm):
+    class Meta(object):
+        model = Photo
+        fields = ['image', 'description', 'tags']
+
+
+class AlbumForm(ModelForm):
+    class Meta(object):
+        model = Album
+        fields = ['title', 'description', 'photos']
+
+
+def frontpage_view():
+    pass
+
+
+def home_view():
+    pass
+
+
+def album_view():
+    pass
+
+
+def photo_view():
+    pass
+
+
+def tag_view():
+    pass
+
+
+def login_view():
+    pass
+
+
+def logout_view():
+    pass
