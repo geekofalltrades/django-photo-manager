fc5b31bd6bb990674a55c386427bb947ef3b9d07
geekofalltrades <thegeekofalltrades@gmail.com>
2014-03-20 17:00:29 -0700

    Prefixed url names with pm-.

--- PhotoManager/urls.py
+++ PhotoManager/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     'PhotoManager.views',
-    url(r'^$', 'frontpage_view', name='frontpage'),
-    url(r'^home/$', 'home_view', name='home'),
-    url(r'^album/(?P<id>\d+)$', 'album_view', name='album'),
-    url(r'^photo/(?P<id>\d+)$', 'photo_view', name='photo'),
-    url(r'^tag/(?P<id>\d+)$', 'tag_view', name='tag'),
-    url(r'^login/$', 'login_view', name='login'),
-    url(r'^logout/$', 'logout_view', name='logout'),
+    url(r'^$', 'frontpage_view', name='pm-front'),
+    url(r'^home/$', 'home_view', name='pm-home'),
+    url(r'^album/(?P<id>\d+)$', 'album_view', name='pm-album'),
+    url(r'^photo/(?P<id>\d+)$', 'photo_view', name='pm-photo'),
+    url(r'^tag/(?P<id>\d+)$', 'tag_view', name='pm-tag'),
+    url(r'^login/$', 'login_view', name='pm-login'),
+    url(r'^logout/$', 'logout_view', name='pm-logout'),
 )
